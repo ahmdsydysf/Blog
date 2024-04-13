8ff3622fd0c196765694366bdefc0e70081ed521
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2024-04-13 21:36:44 +0200

    update 1

--- database/migrations/2024_04_13_192906_create_categories_table.php
+++ database/migrations/2024_04_13_192906_create_categories_table.php
@@ -10,15 +10,13 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', ['user' , 'admin']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image')->nullable();
+            $table->foreignId('parent')->nullable()
+                ->constrained('categories', 'id')->nullOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +25,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
